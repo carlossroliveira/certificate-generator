118a6195a3ff600ade4bde99fb4b499860588b12
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2025-09-23 16:51:50 -0300

    feat: add logo image and update navbar import path in app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core'
-import { Navbar } from "./navbar/navbar"
+import { Navbar } from "./_components/navbar/navbar"
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-root',
-  imports: [ Navbar],
+  imports: [ Navbar, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('certificate-generator');
+  exibirNavbar: boolean = false;
 }
