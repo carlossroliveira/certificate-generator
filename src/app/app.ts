6692fe06b3004f24ba14eb631337465af7f47025
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2025-09-23 17:15:16 -0300

    feat: implement primary and secondary button components with HTML and CSS

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common'
 import { Component, signal } from '@angular/core'
 import { Navbar } from "./_components/navbar/navbar"
-import { CommonModule } from '@angular/common'
+import { PrimaryButton } from "./_components/primary-button/primary-button"
+import { SecondaryButton } from "./_components/secondary-button/secondary-button";
 
 @Component({
   selector: 'app-root',
-  imports: [ Navbar, CommonModule],
+  imports: [Navbar, CommonModule, PrimaryButton, SecondaryButton],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
