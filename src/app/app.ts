897d0a65d84e4ec5ab929171c1be41bda706f682
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2025-09-24 16:49:16 -0300

    feat: create RouterOutlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common'
 import { Component, signal } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
 import { BaseUi } from './_components/base-ui/base-ui'
 import { Navbar } from './_components/navbar/navbar'
-import { Certificado } from './pages/certificado/certificado'
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, CommonModule, BaseUi, Certificado],
+  imports: [Navbar, CommonModule, BaseUi, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
