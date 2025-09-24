698359e42cf0c2faa4a3a6c3d1bc39ceb2b701f0
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2025-09-24 14:24:55 -0300

    feat: update app structure to use certificados-form component and add global styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,11 @@ import { CommonModule } from '@angular/common'
 import { Component, signal } from '@angular/core'
 import { BaseUi } from './_components/base-ui/base-ui'
 import { Navbar } from './_components/navbar/navbar'
-import { Certificados } from './pages/certificados/certificados'
+import { CertificadosForm } from './pages/certificados-form/certificados-form'
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, CommonModule, BaseUi, Certificados],
+  imports: [Navbar, CommonModule, BaseUi, CertificadosForm],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
