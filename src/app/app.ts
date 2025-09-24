3fdd62585ceb4a11adc334c631c1d830677d8159
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2025-09-24 15:28:26 -0300

    feat: replace logo image with SVG version and update component references for certificado

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,11 @@ import { CommonModule } from '@angular/common'
 import { Component, signal } from '@angular/core'
 import { BaseUi } from './_components/base-ui/base-ui'
 import { Navbar } from './_components/navbar/navbar'
-import { CertificadosForm } from './pages/certificados-form/certificados-form'
+import { Certificado } from './pages/certificado/certificado'
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, CommonModule, BaseUi, CertificadosForm],
+  imports: [Navbar, CommonModule, BaseUi, Certificado],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
