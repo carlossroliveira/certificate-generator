d93bcfd6e197b12704654c24073b20e40fef9303
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2025-09-24 11:05:33 -0300

    feat: add item-certificado component with HTML, CSS, and TypeScript files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common'
 import { Component, signal } from '@angular/core'
+import { ItemCertificado } from "./_components/item-certificado/item-certificado"
 import { Navbar } from "./_components/navbar/navbar"
 import { PrimaryButton } from "./_components/primary-button/primary-button"
-import { SecondaryButton } from "./_components/secondary-button/secondary-button";
+import { SecondaryButton } from "./_components/secondary-button/secondary-button"
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, CommonModule, PrimaryButton, SecondaryButton],
+  imports: [Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificado],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
