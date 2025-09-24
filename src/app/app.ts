62c7184fec4a008f99b0368ffa2e302674ae452a
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2025-09-24 11:10:30 -0300

    feat: create base-ui component with HTML, CSS, and TypeScript files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,16 @@
 import { CommonModule } from '@angular/common'
 import { Component, signal } from '@angular/core'
-import { ItemCertificado } from "./_components/item-certificado/item-certificado"
-import { Navbar } from "./_components/navbar/navbar"
-import { PrimaryButton } from "./_components/primary-button/primary-button"
-import { SecondaryButton } from "./_components/secondary-button/secondary-button"
+import { BaseUi } from './_components/base-ui/base-ui'
+import { ItemCertificado } from './_components/item-certificado/item-certificado'
+import { Navbar } from './_components/navbar/navbar'
+import { PrimaryButton } from './_components/primary-button/primary-button'
+import { SecondaryButton } from './_components/secondary-button/secondary-button'
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificado],
+  imports: [Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('certificate-generator');
