0c0a81eb285764a0ae79a9553fd6eddae4fc7cad
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2025-09-24 13:55:34 -0300

    feat: update certificados component to use item-certificado and add list-certificates styling

--- src/app/pages/certificados/certificados.ts
+++ src/app/pages/certificados/certificados.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core'
-import { SecondaryButton } from '../../_components/secondary-button/secondary-button'
+import { ItemCertificado } from '../../_components/item-certificado/item-certificado'
 
 @Component({
   selector: 'app-certificados',
-  imports: [SecondaryButton],
+  imports: [ItemCertificado],
   templateUrl: './certificados.html',
   styleUrl: './certificados.css',
 })
