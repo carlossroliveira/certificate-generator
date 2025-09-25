7f1d6675e2cea58701406d9b2d0e92c60ce6fa47
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2025-09-25 13:30:32 -0300

    feat: using CommonModule and creating validation

--- src/app/pages/certificados-form/certificados-form.ts
+++ src/app/pages/certificados-form/certificados-form.ts
@@ -1,13 +1,21 @@
+import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
-import { PrimaryButton } from "../../_components/primary-button/primary-button"
-import { SecondaryButton } from "../../_components/secondary-button/secondary-button"
+import { FormsModule, NgModel } from '@angular/forms'
+import { PrimaryButton } from '../../_components/primary-button/primary-button'
+import { SecondaryButton } from '../../_components/secondary-button/secondary-button'
 
 @Component({
   selector: 'app-certificados-form',
-  imports: [SecondaryButton, PrimaryButton],
+  imports: [SecondaryButton, PrimaryButton, FormsModule, CommonModule],
   templateUrl: './certificados-form.html',
-  styleUrl: './certificados-form.css'
+  styleUrl: './certificados-form.css',
 })
 export class CertificadosForm {
+  nome: string = '';
+  atividade: string = '';
+  atividades: string[] = ['React', 'Angular', 'Vue'];
 
+  campoInvalido(control: NgModel) {
+    return control.invalid && control.touched;
+  }
 }
