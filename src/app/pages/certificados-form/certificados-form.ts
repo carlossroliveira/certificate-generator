57aa6ba21297b7a26b404fac3cc51e3ffd3ccb0e
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2025-09-25 15:01:16 -0300

    chore: function to add and remove activities

--- src/app/pages/certificados-form/certificados-form.ts
+++ src/app/pages/certificados-form/certificados-form.ts
@@ -1,8 +1,9 @@
-import { CommonModule } from '@angular/common'
-import { Component } from '@angular/core'
-import { FormsModule, NgModel } from '@angular/forms'
-import { PrimaryButton } from '../../_components/primary-button/primary-button'
-import { SecondaryButton } from '../../_components/secondary-button/secondary-button'
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+import { PrimaryButton } from '../../_components/primary-button/primary-button';
+import { SecondaryButton } from '../../_components/secondary-button/secondary-button';
+import { Certificado } from '../interfaces/certificado';
 
 @Component({
   selector: 'app-certificados-form',
@@ -11,15 +12,35 @@ import { SecondaryButton } from '../../_components/secondary-button/secondary-bu
   styleUrl: './certificados-form.css',
 })
 export class CertificadosForm {
-  nome: string = '';
   atividade: string = '';
-  atividades: string[] = ['React', 'Angular', 'Vue'];
+
+  certificado: Certificado = {
+    atividades: [],
+    nome: '',
+  };
 
   campoInvalido(control: NgModel) {
     return control.invalid && control.touched;
   }
 
   formValido() {
-    return this.atividades.length > 0 && this.nome.length > 0;
+    return this.certificado.atividades.length > 0 && this.certificado.nome.length > 0;
+  }
+
+  adicionarAtividade() {
+    this.certificado.atividades.push(this.atividade);
+    this.atividade = '';
+  }
+
+  excluirAtividade(index: number) {
+    this.certificado.atividades.splice(index, 1);
+  }
+
+  submit() {
+    if (!this.formValido()) {
+      return;
+    }
+
+    console.log(this.certificado);
   }
 }
