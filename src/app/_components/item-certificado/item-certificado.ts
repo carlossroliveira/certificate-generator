54a86910881044372747cb5c65a6614ddc7503cf
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2025-09-25 10:48:36 -0300

    chore: using constructor

--- src/app/_components/item-certificado/item-certificado.ts
+++ src/app/_components/item-certificado/item-certificado.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core'
-import { RouterLink } from '@angular/router'
-import { SecondaryButton } from '../secondary-button/secondary-button'
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { SecondaryButton } from '../secondary-button/secondary-button';
 
 @Component({
   selector: 'app-item-certificado',
-  imports: [SecondaryButton, RouterLink],
+  imports: [SecondaryButton],
   templateUrl: './item-certificado.html',
   styleUrl: './item-certificado.css',
 })
 export class ItemCertificado {
   id: string = '1';
+
+  constructor(private router: Router) {}
+
+  redirecionaCertificado() {
+    this.router.navigate(['/certificados', this.id]);
+  }
 }
