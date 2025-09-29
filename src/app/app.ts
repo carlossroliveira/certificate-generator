470e96f701f32f699b29d11f12d989d849d2d8f5
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2025-09-29 15:43:10 -0300

    chore: localStorage implementation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common'
-import { Component, signal } from '@angular/core'
+import { Component, OnInit, signal } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { BaseUi } from './_components/base-ui/base-ui'
 import { Navbar } from './_components/navbar/navbar'
+import { CertificadoService } from './_services/certificado.service'
 
 @Component({
   selector: 'app-root',
@@ -10,7 +11,14 @@ import { Navbar } from './_components/navbar/navbar'
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('certificate-generator');
   exibirNavbar: boolean = false;
+
+  constructor(private certificadoService: CertificadoService) {}
+
+  ngOnInit(): void {
+    const certificados = localStorage.getItem('certificados');
+    this.certificadoService.certificados = certificados ? JSON.parse(certificados) : [];
+  }
 }
