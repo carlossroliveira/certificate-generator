1c84906c8a9b0f5e1a74f56412c79cfe5c0d3692
Carlos Oliveira <63623377+carlossroliveira@users.noreply.github.com>
2025-09-29 15:43:24 -0300

    chore: logic in the Certificados

--- src/app/pages/certificados/certificados.ts
+++ src/app/pages/certificados/certificados.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { RouterModule } from '@angular/router'
 import { ItemCertificado } from "../../_components/item-certificado/item-certificado"
+import { SecondaryButton } from "../../_components/secondary-button/secondary-button"
+import { CertificadoService } from '../../_services/certificado.service'
+import { Certificado } from '../../interfaces/certificado'
 
 @Component({
   selector: 'app-certificados',
-  imports: [ ItemCertificado],
+  imports: [ItemCertificado, SecondaryButton, RouterModule],
   templateUrl: './certificados.html',
   styleUrl: './certificados.css',
 })
-export class Certificados {}
+export class Certificados implements OnInit {
+  certificados: Certificado[] = [];
+
+  constructor(private CertificadoService: CertificadoService) {}
+
+  ngOnInit(): void {
+    this.certificados = this.CertificadoService.certificados;
+  }
+}
